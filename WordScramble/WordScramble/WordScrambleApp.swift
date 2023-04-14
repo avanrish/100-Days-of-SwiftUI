0e208bd60386f8f5075e15a3073a1a32c54fb15e
Bartosz Lipiński <bartosz.s.lipinski@gmail.com>
2023-04-14 22:51:10 +0200

    Project 5

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Bartosz Lipiński on 14/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
