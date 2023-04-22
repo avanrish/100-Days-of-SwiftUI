432059a75e3552e0be3cf7a9d8c6940ff4c90ca5
Bartosz Lipiński <bartosz.s.lipinski@gmail.com>
2023-04-22 16:04:50 +0200

    Project 11

---
+++ Bookworm/Bookworm/AddBookView.swift
@@ -0,0 +1,71 @@
+//
+//  AddBookView.swift
+//  Bookworm
+//
+//  Created by Bartosz Lipiński on 22/04/2023.
+//
+
+import SwiftUI
+
+struct AddBookView: View {
+    @Environment(\.managedObjectContext) var moc
+    @Environment(\.dismiss) var dismiss
+
+    @State private var title = ""
+    @State private var author = ""
+    @State private var rating = 3
+    @State private var genre = "Fantasy"
+    @State private var review = ""
+
+    let genres = ["Fantasy", "Horror", "Kids", "Mystery", "Poetry", "Romance", "Thriller"]
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Name of book", text: $title)
+                    TextField("Author's name", text: $author)
+                    
+                    Picker("Genre", selection: $genre) {
+                        ForEach(genres, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
+                
+                Section("Write a review") {
+                    TextEditor(text: $review)
+                    RatingView(rating: $rating)
+                }
+                
+                Section {
+                    Button("Save") {
+                        let newBook = Book(context: moc)
+                        newBook.id = UUID()
+                        newBook.title = title
+                        newBook.author = author
+                        newBook.rating = Int16(rating)
+                        newBook.genre = genre
+                        newBook.review = review
+                        newBook.date = Date.now
+                        
+                        try? moc.save()
+                        dismiss()
+                    }
+                    .disabled(isDisabled)
+                }
+            }
+            .navigationTitle("Add Book")
+        }
+    }
+    
+    var isDisabled: Bool {
+        title.isEmpty || author.isEmpty
+    }
+}
+
+struct AddBookView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddBookView()
+    }
+}
