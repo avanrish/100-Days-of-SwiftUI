3bb6265df83758584fb68613d92114a9a029db80
Bartosz Lipiński <bartosz.s.lipinski@gmail.com>
2023-04-24 18:37:22 +0200

    Project 13

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Bartosz Lipiński on 23/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
