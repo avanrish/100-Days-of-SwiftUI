8fbe3a5c4171e0119e5a5b62e39755ae9a8a363d
Bartosz Lipiński <bartosz.s.lipinski@gmail.com>
2023-05-01 13:03:36 +0200

    Project 18

---
+++ LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Bartosz Lipiński on 01/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
